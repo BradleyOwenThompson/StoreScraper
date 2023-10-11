3f57d199ca088634cbb1af151307bd93afed9da7
Bradley Thompson <email@email.com>
2023-10-11 20:06:39 +0100

    Updated Readme and main.py example

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from ProductScraper.StoreScraper import StoreScraper
 
 scraper = StoreScraper()
 
-results = scraper.get_information("https://www.argos.co.uk/product/8992866")
-print(results)
+result1 = scraper.get_information("https://www.argos.co.uk/product/8992866")
 
-results = scraper.get_information("https://www.thetoyshop.com/collectibles/adult-collectibles/DC-C"
+result2 = scraper.get_information("https://www.thetoyshop.com/collectibles/adult-collectibles/DC-C"
                                   "omics-30cm-Superman-Figure/p/545842?queryId=83dec2295943aff8586"
                                   "c518c6f031006"
                                   )
-print(results)
+
+print(result1)
+print(result2)
